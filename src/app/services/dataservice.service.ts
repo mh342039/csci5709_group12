60585fcb609f7b8db5f5f7a707ce80e083916f28
Mohammed Hamza Jasnak <mh342039@dal.ca>
2021-07-16 16:53:06 +0530

    update

--- src/app/services/dataservice.service.ts
+++ src/app/services/dataservice.service.ts
@@ -6,14 +6,17 @@ import { Router } from '@angular/router';
 })
 export class DataService {
   loggedInUser: any
+  isAdmin: boolean= false
   constructor(private router: Router) { }
 
   setLoggedInUser(obj: any){
     this.loggedInUser = obj;
+    this.isAdmin = (this.loggedInUser.data.role == "ADMIN")
   }
 
   logout(){
     this.loggedInUser.status = false
+    this.isAdmin = false
     this.router.navigateByUrl('/signin')
   }
   
--- src/app/services/utilityservice.service.ts
+++ src/app/services/utilityservice.service.ts
@@ -10,7 +10,7 @@ export class UtilityService {
   modalTitle:any;
   accordionDetails : any ={};
   opened :boolean = false;
-
+  userRole: string = "Admin"
   isViewMyAnnouncementControlsVisible: boolean= false;
 
   getaccordionDetails(){
